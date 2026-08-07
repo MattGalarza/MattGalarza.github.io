72b7ffe6c476e47259eea13d18f58fca8405f302
MattGalarza <119373372+MattGalarza@users.noreply.github.com>
2026-08-07 18:34:13 +0000

    Deploying to gh-pages from @ MattGalarza/MattGalarza.github.io@2d3c7434092c1a94592bff191c7af89d7d9cb95d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "Education, research, engineering experience, teaching, and technical skills.",
